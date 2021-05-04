c0e100d7c094ebdc92d7f335adb66f6993114536
Carlos Figueiredo <cvieirasp@gmail.com>
2021-05-04 08:32:18 -0300

    first commit

---
+++ src/components/ReviewCard/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
+import { Review } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
+
+import * as S from './styles'
+
+type Props = {
+  id: number
+  review: Review
+}
+
+const ReviewCard: React.FC<Props> = ({ id, review }) => {
+  useEffect(() => {
+    const texts = document.querySelectorAll('p.description')
+
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        entry.target.classList[
+          entry.target.scrollHeight > entry.contentRect.height + 25
+            ? 'add'
+            : 'remove'
+        ]('truncated')
+      }
+    })
+
+    texts.forEach((text) => observer.observe(text))
+  })
+
+  return (
+    <S.Card>
+      <S.User>
+        <S.Image
+          src={getImageUrl(review.photo.url)}
+          alt={review.photo.alternativeText}
+          loading="lazy"
+        />
+        <S.Name>{review.name}</S.Name>
+      </S.User>
+      <S.Text>
+        <input type="checkbox" id={`review-${id}`} />
+        <p className="description">{review.text}</p>
+        <label className="label-more" htmlFor={`review-${id}`}>
+          Ver tudo
+        </label>
+      </S.Text>
+    </S.Card>
+  )
+}
+
+export default ReviewCard
